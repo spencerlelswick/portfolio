63b7fc3fe50a6bb9a0c0007dd71be8a0ca36a186
SpencerlElswick <spencerlelswick@gmail.com>
2023-09-15 02:51:46 -0400

    Adjust about card size

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,8 +16,8 @@ const About = () => {
 
   const loaded = () => (
     <section className='flex justify-center'>
-      <div className='card w-1/2 flex flex-col items-center bg-base-100 shadow-xl'>
-        <div className='flex flex-col items-center my-10 justify-between'>
+      <div className='card bg-base-200  w-3/5 flex flex-col items-center bg-base-100 shadow-xl'>
+        <div className='flex flex-col items-center m-10 justify-between'>
           <h2>Hi, I'm Spencer.</h2>
           <p>I'm a full-stack developer from Louisville, Kentucky.</p>
           <p>
