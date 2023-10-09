c140729311758374b40702bdedcac20b3077f665
SpencerlElswick <spencerlelswick@gmail.com>
2023-10-08 21:45:50 -0400

    Adds link to footer email and hover styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,14 @@ const Footer = () => {
     <footer className='footer p-5 flex flex-row justify-center items-center bg-neutral text-neutral-content'>
       <ul className='flex flex-row justify-center items-center gap-4'>
         <li>About</li>
-        <li>
+        <li className='hover:text-secondary'>
           <a href='/resources/resume.pdf' target='_blank'>
             Resume
           </a>
         </li>
-        <li>Email</li>
+        <li className='hover:text-secondary'>
+          <a href='mailto:spencerelswick@proton.me'>Email</a>
+        </li>
       </ul>
     </footer>
   );
